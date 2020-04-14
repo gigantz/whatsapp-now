164aa361cf29b4e9e0a0b2116c54cec35a3cc9a2
Orkhan Jafarov <orkhanjafarovr@gmail.com>
2020-04-15 01:20:09 +0400

    fix

--- src/visitorInfo.js
+++ src/visitorInfo.js
@@ -1,13 +1,13 @@
-import momentTz from "moment-timezone/builds/moment-timezone-with-data-2012-2022.min";
+import momentTz from 'moment-timezone/builds/moment-timezone-with-data-2012-2022.min';
 import { DEFAULT_COUNTRY } from './constants';
 import { ZONES } from './zones';
 
 export const visitorInfo = () => {
   const timezone = momentTz.tz.guess();
-  const countryCode = (ZONES[timezone] && ZONES[timezone].countries[0]) || DEFAULT_COUNTRY;
+  const countryCode =
+    (ZONES[timezone] && ZONES[timezone][0]) || DEFAULT_COUNTRY;
   return {
     timezone,
     countryCode,
   };
 };
-
